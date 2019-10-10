bab036686bb8781b1fc02e4ebb74b3da5491331b
Gmuchang4685 <muchang19910620@gmail.com>
2019-10-10 12:55:06 -0400

    Test innitial commit

--- ConestogaCarpoolApplication/Controllers/HomeController.cs
+++ ConestogaCarpoolApplication/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace ConestogaCarpoolApplication.Controllers
 
 		public IActionResult About()
 		{
-			ViewData["Message"] = "Your application description page";
+			ViewData["Message"] = "Your application description page........";
 
 			return View();
 		}
